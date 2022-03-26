a03601029f5dadfa859695862c7fed2a1f324d59
Rob Benney <rob.benney@gmail.com>
2022-03-26 11:42:23 -0400

    set current date and add tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(rightNow);
+// set the date at the top of the page
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
+var setTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+var getTasks = function() {
+
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadedTasks) {
+        tasks = loadedTasks
+
+        $.each(tasks, function(hour, task) {
+            var hourDiv = $("#" + hour);
+            createTask(task, hourDiv);
+        })
+    }
+
+
+    auditTasks()
+}
